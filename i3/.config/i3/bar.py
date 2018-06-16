50d9fef2f1d2dc4242c00f3b3e290fa5e7373109
Miguel Madrid Mencía <mimadrid@ucm.es>
2018-06-16 09:47:06 +0200

    i3: Fix bar.py indentation

--- i3/.config/i3/bar.py
+++ i3/.config/i3/bar.py
@@ -8,28 +8,28 @@ base05 = "#f8f8f2"
 # Screen backlight info
 # requires light to change the backlight brightness with the scollwheel.
 status.register("backlight",
-    backlight="radeon_bl0",
-    format=" {percentage}% ",
-    on_upscroll="light -A 1",
-    on_downscroll="light -U 1",
-    interval=1,)
+                backlight="radeon_bl0",
+                format=" {percentage}% ",
+                on_upscroll="light -A 1",
+                on_downscroll="light -U 1",
+                interval=1,)
 
 # Shows pulseaudio default sink volume
 #
 # Note: requires libpulseaudio from PyPI
 status.register("pulseaudio",
-    step=1,
-    format=" {volume}% ",)
+                step=1,
+                format=" {volume}% ",)
 
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
 status.register("clock",
-    format=" %a %-d %b %X KW%V",)
+                format=" %a %-d %b %X KW%V",)
 
 status.register("uptime",
-    format=" {hours}:{mins}",
-    interval=1,)
+                format=" {hours}:{mins}",
+                interval=1,)
 
 # The battery monitor has many formatting options, see README for details
 
@@ -55,46 +55,46 @@ status.register("uptime",
 # This would look like this:
 # Discharging 6h:51m
 status.register("battery",
-    format="{status} {remaining:%E%hh:%Mm}",
-    alert=True,
-    alert_percentage=30,
-    critical_level_percentage=30,
-    interval=1,
-    # $base05
-    full_color=base05,
-    charging_color=base05,
-    status={
-        "DIS":  "",
-        "CHR":  "",
-        "FULL": "",
-    },)
+                format="{status} {remaining:%E%hh:%Mm}",
+                alert=True,
+                alert_percentage=30,
+                critical_level_percentage=30,
+                interval=1,
+                # $base05
+                full_color=base05,
+                charging_color=base05,
+                status={
+                    "DIS":  "",
+                    "CHR":  "",
+                    "FULL": "",
+                },)
 
 
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
 status.register("load",
-    format=" {tasks} {avg1} {avg5} {avg15}",)
+                format=" {tasks} {avg1} {avg5} {avg15}",)
 
 
 #status.register("cpu_usage_graph",
 #    start_color=base05,)
 
 status.register("cpu_freq",
-    format=" {avgg}",
-    interval=1,)
+                format=" {avgg}",
+                interval=1,)
 
 status.register("cpu_usage_bar",
-    bar_type="vertical",
-    format=" {usage_bar}",
-    # $base05
-    start_color=base05,
-    interval=1,)
+                bar_type="vertical",
+                format=" {usage_bar}",
+                # $base05
+                start_color=base05,
+                interval=1,)
 
 # Shows your CPU temperature, if you have a Intel CPU
 status.register("temp",
-    format=" {Package_id_0}°C",
-    lm_sensors_enabled=True,
-    interval=1,)
+                format=" {Package_id_0}°C",
+                lm_sensors_enabled=True,
+                interval=1,)
 
 # Displays whether a DHCP client is running
 #status.register("runwatch",
@@ -109,37 +109,37 @@ status.register("temp",
 #
 # Note: the network module requires PyPI package netifaces
 status.register("network",
-    interface="enp9s0",
-    # $base05
-    color_up=base05,
-    start_color=base05,
-    format_up="{v4cidr}  {bytes_recv}KiB  {bytes_sent}KiB",
-    format_down="",
-    interval=1,)
+                interface="enp9s0",
+                # $base05
+                color_up=base05,
+                start_color=base05,
+                format_up="{v4cidr}  {bytes_recv}KiB  {bytes_sent}KiB",
+                format_down="",
+                interval=1,)
 
 # Note: requires both netifaces and basiciw (for essid and quality)
 status.register("network",
-    interface="wlp8s0",
-    # $base05
-    color_up=base05,
-    start_color=base05,
-    format_up=" {essid} {quality}%  {bytes_recv}KiB  {bytes_sent}KiB",
-    format_down="",
-    interval=1,)
+                interface="wlp8s0",
+                # $base05
+                color_up=base05,
+                start_color=base05,
+                format_up=" {essid} {quality}%  {bytes_recv}KiB  {bytes_sent}KiB",
+                format_down="",
+                interval=1,)
 
 # Shows disk usage of /
 # Format:
 # 42/128G [86G]
 status.register("disk",
-    path="/",
-    format=" {avail}G",
-    on_leftclick="baobab",
-    interval=1,)
+                path="/",
+                format=" {avail}G",
+                on_leftclick="baobab",
+                interval=1,)
 
 # pomodoro timer
 status.register("pomodoro",
-    sound="/usr/share/sounds/freedesktop/stereo/bell.oga",
-    interval=1,)
+                sound="/usr/share/sounds/freedesktop/stereo/bell.oga",
+                interval=1,)
 
 #status.register("updates",
 #                format = "Updates: {count}",
