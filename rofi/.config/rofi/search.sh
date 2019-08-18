17060e2943c722a2376374db58b906e84cc938f0
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2019-08-19 01:26:27 +0200

    rofi: Always close rofi when a url is searched with search.sh

--- rofi/.config/rofi/search.sh
+++ rofi/.config/rofi/search.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$1" ]
-then
+if [ "$1" ]; then
   (firefox-developer-edition --private-window https://www.google.com/search?q="$1" &)
+  # Corner case: Try to open a url without any Firefox instance opened
+  # Result: rofi is never closed by itself
+  if ! "pgrep --full firefox"; then
+    pkill rofi
+  fi
 fi
