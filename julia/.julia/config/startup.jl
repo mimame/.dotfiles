9c9af9db8a7327e9d14d148e20902bbed92c0bbf
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2019-09-15 10:40:19 +0200

    julia: Use Monokai theme by default in the REPL

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,5 +1,10 @@
-# Syntax highlighting and other enhancements for the Julia REPL
+# Syntax highlighting and other enhancements for the Julia REPL with Monokai theme by default
 using OhMyREPL
+colorscheme!("Monokai24bit")
+# Load Debugger with Monokai theme by default
+using Highlights
+using Debugger
+Debugger.set_theme(Highlights.Themes.MonokaiTheme)
 # Automatically update function definitions in a running Julia session
 # Use includet() instead include() to track the files
 atreplinit() do repl
