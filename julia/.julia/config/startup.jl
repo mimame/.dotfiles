ad648210b2d2f8ce452379f1947c844aaaa06cfa
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2019-09-15 10:11:47 +0200

    julia: Automatically update function definitions in a running Julia session

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,1 +1,11 @@
+# Syntax highlighting and other enhancements for the Julia REPL
 using OhMyREPL
+# Automatically update function definitions in a running Julia session
+# Use includet() instead include() to track the files
+atreplinit() do repl
+    try
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch
+    end
+end
