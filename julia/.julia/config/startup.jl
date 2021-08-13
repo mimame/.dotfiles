19bd2d8001d93c2ef81111b873b8a3927b0f0920
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2021-08-13 18:34:38 +0200

    julia: Fix extra `]` when pasting

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,5 +1,10 @@
 # Syntax highlighting and other enhancements for the Julia REPL with Monokai theme by default
 using OhMyREPL
+@async begin
+    # reinstall keybindings to work around https://github.com/KristofferC/OhMyREPL.jl/issues/166
+    sleep(1)
+    OhMyREPL.Prompt.insert_keybindings()
+end
 colorscheme!("Monokai24bit")
 # Load Debugger with Monokai theme by default
 using Highlights
