184938f1eccc60818f6bf4cac821c3e8f8d2487e
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2021-08-31 17:15:25 +0200

    nvim: Add missing nvim-autopairs config to cmp

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,5 +1,11 @@
 local cmp = require('cmp')
 
+require("nvim-autopairs.completion.cmp").setup({
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true, -- it will auto insert `(` after select function or method item
+  auto_select = true -- automatically select the first item
+})
+
 local lspkind = require('lspkind')
 
 -- nvim-cmp supports additional completion capabilities
