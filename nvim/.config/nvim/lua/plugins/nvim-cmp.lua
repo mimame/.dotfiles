16573db3be977a12a0161cd30cf05a23e3a293cc
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2021-09-24 18:06:16 +0200

    nvim: Update nvim-cmp config to the last version

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,24 +13,28 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 cmp.setup({
-  formatting = {
+})
+
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+cmp.setup({
+    formatting = {
     format = function(entry, vim_item)
       vim_item.kind = lspkind.presets.default[vim_item.kind]
       return vim_item
     end,
   },
-})
 
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
-cmp.setup({
   snippet = {
     expand = function(args)
       -- You must install `vim-vsnip` if you use the following as-is.
@@ -52,31 +56,22 @@ cmp.setup({
     }),
     ['<tab>'] = cmp.mapping(function(fallback)
       if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(t('<C-n>'), 'n')
-        -- elseif vim.fn['vsnip#jumpable'](1)  then
-      elseif vim.fn['vsnip#available']() == 1 then
-        vim.fn.feedkeys(t('<Plug>(vsnip-expand-or-jump)'), '')
-      elseif check_back_space() then
-        vim.fn.feedkeys(t('<tab>'), 'n')
+        feedkey("<C-n>", "n")
+      elseif vim.fn["vsnip#available"]() == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
       else
-        fallback()
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
-    end, {
-      'i',
-      's',
-    }),
-    ['<S-tab>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
       if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(t('<C-p>'), 'n')
-      elseif vim.fn['vsnip#jumpable'](-1) then
-        vim.fn.feedkeys(t('<Plug>(vsnip-jump-prev)'), '')
-      else
-        fallback()
+        feedkey("<C-p>", "n")
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
-    end, {
-      'i',
-      's',
-    }),
+    end, { "i", "s" }),
   },
   -- You should specify your *installed* sources.
   sources = {
