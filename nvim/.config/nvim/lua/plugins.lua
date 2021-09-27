83b905944552d9124d14a9fd3ee3b72ca91ce094
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2021-09-27 17:57:12 +0200

    nvim: Use bufferline.nvim instead barbar.nvim plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -278,76 +278,77 @@ return require('packer').startup(function()
     end,
   })
 
-  -- A neovim tabline plugin
-  use({
-    'romgrk/barbar.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
+  -- A snazzy bufferline for Neovim
+  use {
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
     config = function()
-      -- Set barbar's options
-      vim.g.bufferline = {
-        -- Enable/disable animations
-        animation = true,
-        -- Enable/disable auto-hiding the tab bar when there is a single buffer
-        auto_hide = false,
-        -- Enable/disable current/total tabpages indicator (top right corner)
-        tabpages = true,
-        -- Enable/disable close button
-        closable = true,
-        -- Enables/disable clickable tabs
-        --  - left-click: go to buffer
-        --  - middle-click: delete buffer
-        clickable = true,
-        -- Enable/disable icons
-        -- if set to 'numbers', will show buffer index in the tabline
-        -- if set to 'both', will show buffer index and icons in the tabline
-        icons = 'both',
-        -- If set, the icon color will follow its corresponding buffer
-        -- highlight group. By default, the Buffer*Icon group is linked to the
-        -- Buffer* group (see Highlighting below). Otherwise, it will take its
-        -- default value as defined by devicons.
-        icon_custom_colors = false,
-
-        -- Configure icons on the bufferline.
-        icon_separator_active = '▎',
-        icon_separator_inactive = '▎',
-        icon_close_tab = '',
-        icon_close_tab_modified = '●',
-        icon_pinned = '車',
-
-        -- If true, new buffers will be inserted at the end of the list.
-        -- Default is to insert after current buffer.
-        insert_at_end = false,
-
-        -- Sets the maximum padding width with which to surround each tab
-        maximum_padding = 1,
-
-        -- Sets the maximum buffer name length.
-        maximum_length = 30,
-
-        -- If set, the letters for each buffer in buffer-pick mode will be
-        -- assigned based on their name. Otherwise or in case all letters are
-        -- already assigned, the behavior is to assign letters in order of
-        -- usability (see order below)
-        semantic_letters = true,
-
-        -- New buffer letters are assigned in this order. This order is
-        -- optimal for the qwerty keyboard layout but might need adjustement
-        -- for other layouts.
-        letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-
-        -- Sets the name of unnamed buffers. By default format is "[Buffer X]"
-        -- where X is the buffer number. But only a static string is accepted here.
-        no_name_title = nil,
+      require('bufferline').setup {
+        options = {
+          numbers = function(opts)
+            return string.format('%s', opts.ordinal)
+          end,
+          close_command = "bdelete! %d",
+          right_mouse_command = "bdelete! %d",
+          left_mouse_command = "buffer %d",
+          middle_mouse_command = nil,
+          -- NOTE: this plugin is designed with this icon in mind,
+          -- and so changing this is NOT recommended, this is intended
+          -- as an escape hatch for people who cannot bear it for whatever reason
+          indicator_icon = '▎',
+          buffer_close_icon = '',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+          --- name_formatter can be used to change the buffer's label in the bufferline.
+          --- Please note some names can/will break the
+          --- bufferline so use this at your discretion knowing that it has
+          --- some limitations that will *NOT* be fixed.
+          name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
+            -- remove extension from markdown files for example
+            if buf.name:match('%.md') then
+              return vim.fn.fnamemodify(buf.name, ':t:r')
+            end
+          end,
+          max_name_length = 18,
+          max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+          tab_size = 18,
+          diagnostics = "nvim_lsp",
+          diagnostics_update_in_insert = false,
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and " "
+              or (e == "warning" and " " or "" )
+              s = s .. n .. sym
+            end
+            return s
+          end,
+          offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left" }},
+          show_buffer_icons = true,
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+          show_tab_indicators = true,
+          persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+          -- can also be a table containing 2 custom separators
+          -- [focused and unfocused]. eg: { '|', '|' }
+          separator_style = "thin",
+          -- enforce_regular_tabs = false | true,
+          always_show_bufferline = true,
+          sort_by = 'id',
+        }
       }
-    end,
-  })
-  -- A neovim lua plugin to help easily manage multiple terminal windows
-  use({
-    'akinsho/toggleterm.nvim',
-    config = function()
-      require('toggleterm').setup({
-        open_mapping = [[<c-t>]],
-        -- insert_mappings = true, -- whether or not the open mapping applies in insert mode
+    end
+  }
+
+    -- A neovim lua plugin to help easily manage multiple terminal windows
+    use({
+      'akinsho/toggleterm.nvim',
+      config = function()
+        require('toggleterm').setup({
+          open_mapping = [[<c-t>]],
+          -- insert_mappings = true, -- whether or not the open mapping applies in insert mode
         start_in_insert = true,
         hide_numbers = true, -- hide the number column in toggleterm buffers
         direction = 'tab',
