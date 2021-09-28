81f459b052b4e61c066749378368972a9bfbeb07
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2021-09-28 15:20:52 +0200

    nvim: Add cmp tmux completion

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -112,6 +112,7 @@ return require('packer').startup(function()
       { 'hrsh7th/vim-vsnip-integ' },
       { 'rafamadriz/friendly-snippets' },
       { 'hrsh7th/cmp-nvim-lsp' },
+      { 'andersevenrud/compe-tmux', branch = 'cmp'},
     },
     config = function()
       require('./plugins/nvim-cmp')
--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -85,6 +85,6 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'vsnip' },
-    -- { name = 'tmux' }
+    { name = 'tmux' },
   },
 })
