99c303c04bfe40a6ecf8c37d11b7be6487659d46
Miguel Madrid Mencía <miguel.madrid.mencia@gmail.com>
2021-10-21 12:16:10 +0200

    nvim: Add git blame line to which-key mappings

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -66,6 +66,7 @@ wk.register({
     u = { '<cmd>lua require"gitsigns.actions".undo_stage_hunk()<cr>', 'Undo stage hunk' },
     r = { '<cmd>lua require"gitsigns.actions".reset_hunk()<cr>', 'Undo stage hunk' },
     P = { '<cmd>lua require"gitsigns.actions".preview_hunk()<cr>', 'Preview hunk' },
+    B = { '<cmd>lua require"gitsigns.actions".blame_line(true)<cr>', 'Git blame line' },
   },
 }, {
   prefix = '<leader>',
