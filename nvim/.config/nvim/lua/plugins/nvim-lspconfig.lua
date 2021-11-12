09663181023e4dd3a58c3599a2825eadd12141b3
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2021-11-12 23:10:10 +0100

    refactor(nvim): Use default julials config implicitly

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -30,6 +30,7 @@ local servers = {
   'html',
   'jsonls',
   'jedi_language_server',
+  'julials',
   'pyright',
   'vimls',
   'yamlls',
@@ -52,48 +53,6 @@ end
 -- when snippet support is enabled. To enable completion,
 -- install a snippet plugin and add the following override
 -- to your language client capabilities during setup.
-require('lspconfig').julials.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_new_config = function(new_config, new_root_dir)
-    server_path = '/home/paradise/.julia/packages/LanguageServer/JrIEf/src/LanguageServer.jl/src'
-    cmd = {
-      'julia',
-      '--project=' .. server_path,
-      '--startup-file=no',
-      '--history-file=no',
-      '-e',
-      [[
-      using Pkg
-      Pkg.instantiate()
-      using LanguageServer
-      depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
-      project_path = let
-      dirname(something(
-      ## 1. Finds an explicitly set project (JULIA_PROJECT)
-      Base.load_path_expand((
-      p = get(ENV, "JULIA_PROJECT", nothing);
-      p === nothing ? nothing : isempty(p) ? nothing : p
-      )),
-      ## 2. Look for a Project.toml file in the current working directory,
-      ##    or parent directories, with $HOME as an upper boundary
-      Base.current_project(),
-      ## 3. First entry in the load path
-      get(Base.load_path(), 1, nothing),
-      ## 4. Fallback to default global environment,
-      ##    this is more or less unreachable
-      Base.load_path_expand("@v#.#"),
-      ))
-    end
-    @info "Running language server" VERSION pwd() project_path depot_path
-    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
-    server.runlinter = true
-    run(server)
-    ]],
-    }
-    new_config.cmd = cmd
-  end,
-})
 
 require('lspconfig').texlab.setup({
   settings = {
