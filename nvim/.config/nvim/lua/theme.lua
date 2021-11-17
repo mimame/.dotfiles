d18ed46f8af8e27561d79ec1a33c21f41dc8432d
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2021-11-17 10:50:56 +0100

    feat(nvim): Color diagnostic virtual text

--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -1,12 +1,3 @@
 cmd('colorscheme sonokai')
--- cmd 'highlight! Normal guibg=#000000'
--- cmd 'highlight! Normal ctermbg=0'
--- cmd 'highlight! LineNr cterm=bold guifg=#FFFFFF guibg=#000000 cterm=bold gui=bold'
--- cmd 'highlight! Comment guifg=#C0C0C0'
--- cmd 'highlight! Comment ctermfg=8'
---
--- -- Take green cursor from terminal (Alacritty)
--- cmd 'highlight! CursorLine   ctermbg=none guibg=#403D3D'
--- cmd 'highlight! CursorLineNr ctermbg=236 cterm=bold guibg=#232526 gui=bold'
-
 g.sonokai_transparent_background = 1
+g.sonokai_diagnostic_virtual_text = 'colored'
