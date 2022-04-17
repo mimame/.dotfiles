af3f93d91d4817734a20d2f8e9486d83fe80456b
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-04-17 17:27:44 +0200

    feat(nvim): Use luasnip instead vsnip and update sources preference

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -159,17 +159,25 @@ return require('packer').startup(function()
     end,
   })
 
+
+  --  Snippet Engine for Neovim written in Lua.
+  use({
+    'L3MON4D3/LuaSnip',
+    requires = { { 'honza/vim-snippets' }, { 'rafamadriz/friendly-snippets' } },
+    config = function()
+      require('luasnip.loaders.from_snipmate').lazy_load()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  })
+
   -- A completion plugin for neovim coded by Lua
   use({
     'hrsh7th/nvim-cmp',
     requires = {
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-nvim-lua' },
-      { 'hrsh7th/cmp-vsnip' },
       { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/vim-vsnip' },
-      { 'hrsh7th/vim-vsnip-integ' },
-      { 'rafamadriz/friendly-snippets' },
+      { 'saadparwaiz1/cmp_luasnip' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'andersevenrud/cmp-tmux' },
       { 'kdheepak/cmp-latex-symbols' },
@@ -208,7 +216,7 @@ return require('packer').startup(function()
               vim_item.kind = lspkind.presets.default[vim_item.kind] .. ' ' .. vim_item.kind
               vim_item.menu = ({
                 nvim_lsp = '[LSP]',
-                vsnip = '[Snippet]',
+                luasnip = '[Snippet]',
                 treesitter = '[Treesitter]',
                 buffer = '[Buffer]',
                 path = '[Path]',
--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,9 +13,7 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
+local luasnip = require('luasnip')
 
 cmp.setup({
   formatting = {
@@ -34,70 +32,60 @@ cmp.setup({
 
   snippet = {
     expand = function(args)
-      -- You must install `vim-vsnip` if you use the following as-is.
-      vim.fn['vsnip#anonymous'](args.body)
+      require('luasnip').lsp_expand(args.body)
     end,
   },
 
   -- You must set mapping if you want.
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn['vsnip#available']() == 1 then
-        feedkey('<Plug>(vsnip-expand-or-jump)', '')
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
       else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        fallback()
       end
-    end, {
-      'i',
-      's',
-    }),
-
-    ['<S-Tab>'] = cmp.mapping(function()
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-        feedkey('<Plug>(vsnip-jump-prev)', '')
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
-    end, {
-      'i',
-      's',
-    }),
-  },
+    end, { 'i', 's' }),
+  }),
   -- You should specify your *installed* sources.
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'treesitter' },
-    { name = 'buffer' },
+    { name = 'luasnip' },
     { name = 'path' },
-    { name = 'tmux' },
-    { name = 'look', keyword_length = 3, max_item_count = 10 },
     { name = 'nvim_lua' },
     { name = 'latex_symbols' },
     -- { name = 'dictionary', keyword_length = 3 },
     -- { name = 'nuspell' },
     -- { name = 'spell' },
+  }, {
+    { name = 'buffer' },
+    { name = 'treesitter' },
+    { name = 'tmux' },
+  }, {
+    { name = 'look', keyword_length = 3, max_item_count = 10 },
   }),
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' },
   },
@@ -105,6 +93,7 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' },
   }, {
