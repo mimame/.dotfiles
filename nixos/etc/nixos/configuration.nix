eacabd89f4dad84bc192311714984e239b50c7ef
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-11-22 01:49:44 +0100

    fix(nixos): Add network-manager

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
