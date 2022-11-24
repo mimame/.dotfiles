8fff59ec9e71faf8c7cc23387d4bcbc856c961c7
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-11-24 20:18:57 +0100

    feat(nixos): Initial migration to sway and wayland

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -6,6 +6,45 @@
 let
   unstableTarball =
     fetchTarball "https://github.com/nixos/nixpkgs/tarball/nixos-unstable";
+
+  # bash script to let dbus know about important env variables and
+  # propagate them to relevent services run at the end of sway config
+  # see
+  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
+  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
+  # some user services to make sure they have the correct environment variables
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+  # currently, there is some friction between sway and gtk:
+  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+  # the suggested way to set gtk settings is with gsettings
+  # for gsettings to work, we need to tell it where the schemas are
+  # using the XDG_DATA_DIR environment variable
+  # run at the end of sway config
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text = let
+      schema = pkgs.gsettings-desktop-schemas;
+      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+    in ''
+      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+      gnome_schema=org.gnome.desktop.interface
+      gsettings set $gnome_schema gtk-theme 'Sweet'
+    '';
+  };
+
 in {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -40,8 +79,6 @@ in {
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.utf8";
 
-  networking.firewall.enable = true;
-
   # Synchronise time and date
   services.chrony.enable = true;
   services.localtime.enable = true;
@@ -53,7 +90,25 @@ in {
   services.locate.localuser = null;
 
   # Enable the X11 windowing system.
+  # services.xserver.enable = true;
   services.xserver.enable = true;
+  services.xserver.displayManager = {
+    gdm.enable = true;
+    defaultSession = "sway";
+    autoLogin = {
+      enable = true;
+      user = "mimame";
+    };
+  };
+  services.xserver.libinput.enable = true;
+
+  services.interception-tools.enable = true;
+  services.interception-tools.udevmonConfig = ''
+    - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+      DEVICE:
+        EVENTS:
+          EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+  '';
 
   # Configure keymap in X11
   services.xserver = {
@@ -85,14 +140,14 @@ in {
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.defaultUserShell = pkgs.unstable.fish;
   users.users.mimame = {
     isNormalUser = true;
     description = "mimame";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "video" ];
     packages = with pkgs;
       [
         #  firefox
@@ -101,17 +156,19 @@ in {
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager = {
-    lightdm.enable = true;
-    autoLogin = {
-      enable = true;
-      user = "mimame";
-    };
-  };
+  # services.xserver.displayManager = {
+  #   lightdm.enable = true;
+  #   autoLogin = {
+  #     enable = true;
+  #     user = "mimame";
+  #   };
+  # };
 
-  services.xserver.windowManager.i3.enable = true;
+  # services.xserver.windowManager.i3.enable = true;
 
   services.gnome.gnome-keyring.enable = true;
+
+  security.polkit.enable = true;
 
   # TODO: fix locate.latitude
   # for that simply use geoclue2 in the config file instead a fixed latitude
@@ -170,7 +227,7 @@ in {
     dstat
     dua
     duf
-    dunst
+    # dunst
     dura
     entr
     unstable.espanso
@@ -211,7 +268,6 @@ in {
     gvfs
     gzip
     handlr
-    haskellPackages.greenclip
     unstable.helix
     hexyl
     htop
@@ -219,7 +275,7 @@ in {
     hugo
     unstable.hyperfine
     hyphen
-    i3lock
+    # i3lock
     i3status-rust
     inkscape
     jc
@@ -235,6 +291,7 @@ in {
     less
     libarchive
     libreoffice-fresh
+    light
     linuxKernel.kernels.linux_zen
     linuxKernel.packages.linux_zen.virtualbox
     litecli
@@ -270,6 +327,7 @@ in {
     unstable.nushell
     unstable.obsidian
     onefetch
+    openssl
     pandoc
     parallel
     parted
@@ -351,18 +409,18 @@ in {
     vlc
     unstable.vscode
     watchexec
-    wget
-    wmctrl
-    xautolock
-    xbanish
-    unstable.xcape
-    xclip
+    # wget
+    # wmctrl
+    # xautolock
+    # xbanish
+    # unstable.xcape
+    # xclip
     xdg-user-dirs
     xdg-utils
-    xdotool
+    # xdotool
     unstable.xh
     unstable.xonsh
-    xsel
+    # xsel
     yarn
     zathura
     unstable.zellij
@@ -373,6 +431,22 @@ in {
     unstable.zotero
     unstable.zoxide
     unstable.zstd
+
+    interception-tools
+    libevdev
+    sway
+    dbus-sway-environment
+    configure-gtk
+    wayland
+    wlrctl
+    swaylock
+    swayidle
+    ydotool
+    clipman
+    grim # screenshot functionality
+    slurp # screenshot functionality
+    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+    mako # notification system developed by sway
 
   ];
 
@@ -389,6 +463,34 @@ in {
     enableSSHSupport = true;
   };
 
+  # xdg-desktop-portal works by exposing a series of D-Bus interfaces
+  # known as portals under a well-known name
+  # (org.freedesktop.portal.Desktop) and object path
+  # (/org/freedesktop/portal/desktop).
+  # The portal interfaces include APIs for file access, opening URIs,
+  # printing and others.
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+  };
+
+  # enable sway window manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
@@ -398,10 +500,11 @@ in {
   };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Open barrier 24800 port
+  networking.firewall.allowedTCPPorts = [ 24800 ];
+  networking.firewall.allowedUDPPorts = [ 24800 ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
