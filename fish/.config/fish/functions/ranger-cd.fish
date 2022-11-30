3490401fdcb9403682c8473ea4687f7adc4f18c1
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-11-30 18:44:28 +0000

    feat(fish): Add ranger-cd function

---
+++ fish/.config/fish/functions/ranger-cd.fish
@@ -0,0 +1,9 @@
+function ranger-cd
+    set dir (mktemp -t ranger_cd.XXX)
+    ranger --choosedir=$dir
+    cd (cat $dir) $argv
+    rm $dir
+    commandline -f repaint
+end
+bind -M insert \co ranger-cd
+
