6d931a1d24c5b79c22a0f11d2f219f3970f3c22a
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-12-16 13:21:25 -0600

    fix(fish): Remove problematic python function

--- fish/.config/fish/functions/python.fish
+++
@@ -1,9 +0,0 @@
-function python
-    #   # stdin is a pipe
-    if command test -p /dev/stdin
-        command python "$argv"
-    else
-        # Use command to avoid the recursion
-        test -z "$argv" && ptpython || command python "$argv"
-    end
-end
