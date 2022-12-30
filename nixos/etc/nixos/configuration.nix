6595298d643b8d8b19e6ead9038da83ea71894d7
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-12-29 23:17:04 -0600

    fix(nixos): Restart pipewire services instead stop then start

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -20,8 +20,7 @@ let
 
     text = ''
       dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user restart pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
     '';
   };
 
