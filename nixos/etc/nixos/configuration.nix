66c3cfc0eb1d41d7df788aa241cd00990c9de774
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2022-12-30 19:44:31 -0600

    fix(nixos): Fix scaling CPU governor ondemand

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -11,7 +11,7 @@ let
   # propagate them to relevent services run at the end of sway config
   # see
   # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
-  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts
+  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
   # some user services to make sure they have the correct environment variables
   dbus-sway-environment = pkgs.writeTextFile {
     name = "dbus-sway-environment";
@@ -101,8 +101,8 @@ in {
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # FIXME: Not working
-  powerManagement.cpuFreqGovernor = "ondemand";
+  # Improve battery scaling the CPU governor and optimizing the general power
+  services.auto-cpufreq.enable = true;
 
   system.autoUpgrade = {
     allowReboot = false;
