256d7c024f7a8086a3cbc20cbebaaafce9d728dd
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-01-06 15:47:00 -0600

    feat(wezterm): Enable Wayland

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ return {
   -- && sudo tic -x -o /usr/share/terminfo $tempfile \
   -- && rm $tempfile
   exit_behavior = "Close",
-  -- hide_tab_bar_if_only_one_tab = true,
+  enable_wayland = true,
   font = wezterm.font("Hack Nerd Font", { weight="Regular"}),
   font_size = 18.0,
   color_scheme = "Catppuccin Mocha",
