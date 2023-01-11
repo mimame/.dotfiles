7df5a055a638b0cbb8923c766056502561e7824f
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-01-10 19:34:25 -0600

    fix(wezterm): Add copy on select

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,14 @@
 local wezterm = require 'wezterm';
 
+-- https://github.com/wez/wezterm/issues/2588#issuecomment-1268054635
+function make_mouse_binding(dir, streak, button, mods, action)
+  return {
+    event = { [dir] = { streak = streak, button = button } },
+    mods = mods,
+    action = action,
+  }
+end
+
 return {
  term = "wezterm",
   -- tempfile=$(mktemp) \
@@ -22,5 +31,21 @@ return {
     right = 0,
     top = 0,
     bottom = 0,
-  }
+  },
+  keys = {
+      {
+        key = 'C',
+        mods = 'CTRL',
+        action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection',
+      },
+    },
+  -- Add copy on select
+  mouse_bindings = {
+      make_mouse_binding('Up', 1, 'Left', 'NONE', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 1, 'Left', 'SHIFT', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 1, 'Left', 'ALT', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 1, 'Left', 'SHIFT|ALT', wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 2, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+      make_mouse_binding('Up', 3, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+    },
 }
