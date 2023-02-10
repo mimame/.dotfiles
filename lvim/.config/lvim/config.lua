e5291a812b77409f2b77f8d743ce702ac7516139
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-02-10 21:58:08 +0100

    feat(lvim): Use icons for git status in nvimtree

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -74,7 +74,7 @@ lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "right"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
