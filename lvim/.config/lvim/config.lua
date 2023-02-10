c7eece1474658ea54d98833a7756b64cd36ceed6
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-02-10 21:59:17 +0100

    fix(lvim): Install automatically all treesitter and LSP servers

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -76,22 +76,23 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.autopairs.active = true
+lvim.lsp.automatic_servers_installation = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-}
+-- lvim.builtin.treesitter.ensure_installed = {
+--   "bash",
+--   "c",
+--   "javascript",
+--   "json",
+--   "lua",
+--   "python",
+--   "typescript",
+--   "tsx",
+--   "css",
+--   "rust",
+--   "java",
+--   "yaml",
+-- }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
