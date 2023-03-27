2e78477769b5aff92b80f7a8751ecded1f132896
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-03-27 16:20:20 +0200

    fix(fish): Remove deprecated and redundant abbr flags

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -1,146 +1,146 @@
-abbr -a -U sfish source ~/.config/fish/config.fish
-abbr -a !! --position anywhere --function last_history_item
-abbr -a -U cat bat
+abbr sfish source ~/.config/fish/config.fish
+abbr !! --position anywhere --function last_history_item
+abbr cat bat
 alias ls 'exa --sort .name --color=always --long --links --group --git --icons --classify --extended --ignore-glob=node_modules --all'
-abbr -a -U ll ls
-abbr -a -U l br
-abbr -a -U cd z
-abbr -a -U b 'cd ..'
-abbr --add dotdot --regex '^\.\.+$' --function multicd
-abbr -a -U ju juliaup
-abbr -a -U ru rustup
-abbr -a -U n nim
-abbr -a -U cr crystal
-abbr -a -U g git
-abbr -a -U lg lazygit
-abbr -a -U pc pre-commit
-abbr -a -U t " t"
-abbr -a -U open o
-abbr -a -U ff ' ff'
-abbr -a -U fk 'fk --yes'
-abbr -a -U md 'mkdir -pv'
-abbr -a -U mkdir 'mkdir -pv'
-abbr -a -U pgrep 'pgrep -f'
-abbr -a -U pkill 'pkill -9 -f'
-abbr -a -U k 'pkill -9 -f'
-abbr -a -U bc 'bc -l'
-abbr -a -U mk 'mkdir -pv'
-abbr -a -U u topgrade
+abbr ll ls
+abbr l br
+abbr cd z
+abbr b 'cd ..'
+abbr dotdot --regex '^\.\.+$' --function multicd
+abbr ju juliaup
+abbr ru rustup
+abbr n nim
+abbr cr crystal
+abbr g git
+abbr lg lazygit
+abbr pc pre-commit
+abbr t " t"
+abbr open o
+abbr ff ' ff'
+abbr fk 'fk --yes'
+abbr md 'mkdir -pv'
+abbr mkdir 'mkdir -pv'
+abbr pgrep 'pgrep -f'
+abbr pkill 'pkill -9 -f'
+abbr k 'pkill -9 -f'
+abbr bc 'bc -l'
+abbr mk 'mkdir -pv'
+abbr u topgrade
 
-abbr -a -U docker podman
+abbr docker podman
 
-abbr -a -U p 'ptipython'
+abbr p 'ptipython'
 
-abbr -a -U grep 'grep --color=auto'
-abbr -a -U fgrep 'fgrep --color=auto'
-abbr -a -U egrep 'egrep --color=auto'
+abbr grep 'grep --color=auto'
+abbr fgrep 'fgrep --color=auto'
+abbr egrep 'egrep --color=auto'
 
-abbr -a -U x 'chmod +x'
+abbr x 'chmod +x'
 
-abbr -a -U sshfs "sshfs -o allow_other,default_permissions,follow_symlinks,kernel_cache,reconnect,ServerAliveInterval=60,ServerAliveCountMax=3"
+abbr sshfs "sshfs -o allow_other,default_permissions,follow_symlinks,kernel_cache,reconnect,ServerAliveInterval=60,ServerAliveCountMax=3"
 
 # systemctl
-abbr -a -U sstatus 'sudo systemctl status'
-abbr -a -U srestart 'sudo systemctl restart'
-abbr -a -U sstart 'sudo systemctl start'
-abbr -a -U senable 'sudo systemctl enable'
-abbr -a -U sdisable 'sudo systemctl disable'
-abbr -a -U sstop 'sudo systemctl stop'
+abbr sstatus 'sudo systemctl status'
+abbr srestart 'sudo systemctl restart'
+abbr sstart 'sudo systemctl start'
+abbr senable 'sudo systemctl enable'
+abbr sdisable 'sudo systemctl disable'
+abbr sstop 'sudo systemctl stop'
 
-abbr -a -U a ansible
-abbr -a -U ap ansible-playbook
+abbr a ansible
+abbr ap ansible-playbook
 
-abbr -a -U vbm VBoxManage
+abbr vbm VBoxManage
 
 # Never use vi
-abbr -a -U vi lvim
-abbr -a -U vim lvim
-abbr -a -U v lvim
-abbr -a -U nvim lvim
+abbr vi lvim
+abbr vim lvim
+abbr v lvim
+abbr nvim lvim
 # Always preserve the environment with sudoedit
-abbr -a -U sv sudoedit
-abbr -a -U se sudoedit
+abbr sv sudoedit
+abbr se sudoedit
 
-abbr -a -U h hx
+abbr h hx
 
-abbr -a -U nd 'nvim -d -c "set nofoldenable"'
-# abbr -a -U h 'history' # Use ctrl-r instead
+abbr nd 'nvim -d -c "set nofoldenable"'
+# abbr h 'history' # Use ctrl-r instead
 # Lists the ten most used commands.
-abbr -a -U hs "history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | bat --number"
-abbr -a -U du 'du -h'
-abbr -a -U dus diskus
-abbr -a -U df 'df -h'
-abbr -a -U free 'free -h'
-# abbr -a -U j "jobs -l"
-abbr -a -U j just
-abbr -a -U ncdu 'ncdu --color dark'
-abbr -a -U news newsboat
-abbr -a -U fm vifm
-abbr -a -U r ranger
-abbr -a -U re massren
-abbr -a -U T 'tail -F'
-abbr -a -U nano micro
-abbr -a -U less bat
-abbr -a -U more bat
-abbr -a -U m man
+abbr hs "history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | bat --number"
+abbr du 'du -h'
+abbr dus diskus
+abbr df 'df -h'
+abbr free 'free -h'
+# abbr j "jobs -l"
+abbr j just
+abbr ncdu 'ncdu --color dark'
+abbr news newsboat
+abbr fm vifm
+abbr r ranger
+abbr re massren
+abbr T 'tail -F'
+abbr nano micro
+abbr less bat
+abbr more bat
+abbr m man
 
 # rg with grep -r behavior
-abbr -a -U ag 'ag --smart-case --ignore node_modules'
-abbr -a -U s 'rg --smart-case --no-heading --with-filename --hidden --ignore-file ~/.config/fd/ignore'
+abbr ag 'ag --smart-case --ignore node_modules'
+abbr s 'rg --smart-case --no-heading --with-filename --hidden --ignore-file ~/.config/fd/ignore'
 
 # fd with find behavior
-abbr -a -U f 'fd --hidden --strip-cwd-prefix'
+abbr f 'fd --hidden --strip-cwd-prefix'
 
 # Safe ops. Ask the user before doing anything destructive.
 # Move rm files to the trash using trash-cli
-abbr -a -U rm trash
-abbr -a -U mv "mv -i"
-abbr -a -U cp "cp -ri"
-abbr -a -U ln "ln -i"
+abbr rm trash
+abbr mv "mv -i"
+abbr cp "cp -ri"
+abbr ln "ln -i"
 
-# abbr -a -U for rc files
-abbr -a -U brootrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/broot/conf.toml) && popd'
-abbr -a -U clifmrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/clifm/profiles/default/clifmrc) && popd'
-abbr -a -U dunstrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/dunst/dunstrc) && popd'
-abbr -a -U fishrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/fish/config.fish) && popd'
-abbr -a -U gitrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/git/config) && popd'
-abbr -a -U kittyrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/kitty/kitty.conf) && popd'
-abbr -a -U mimerc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/mimeapps.list) && popd'
-abbr -a -U newsboatrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/newsboat/config) && popd'
-abbr -a -U nixosrc 'sudoedit /etc/nixos/configuration.nix && sudo nixfmt /etc/nixos/configuration.nix && sudo nixos-rebuild switch && cp /etc/nixos/configuration.nix ~/.dotfiles/nixos/etc/nixos/'
-abbr -a -U rofirc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/rofi/config.rasi) && popd'
-abbr -a -U sshrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.ssh/config) && popd'
-abbr -a -U swayrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/sway/config) && popd'
-abbr -a -U tridactylrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/tridactyl/tridactylrc) && popd'
-abbr -a -U weztermrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/wezterm/wezterm.lua) && popd'
-abbr -a -U xplrrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/xplr/init.lua) && popd'
-abbr -a -U zellijrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/zellij/config.kdl) && popd'
+# abbr for rc files
+abbr brootrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/broot/conf.toml) && popd'
+abbr clifmrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/clifm/profiles/default/clifmrc) && popd'
+abbr dunstrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/dunst/dunstrc) && popd'
+abbr fishrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/fish/config.fish) && popd'
+abbr gitrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/git/config) && popd'
+abbr kittyrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/kitty/kitty.conf) && popd'
+abbr mimerc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/mimeapps.list) && popd'
+abbr newsboatrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/newsboat/config) && popd'
+abbr nixosrc 'sudoedit /etc/nixos/configuration.nix && sudo nixfmt /etc/nixos/configuration.nix && sudo nixos-rebuild switch && cp /etc/nixos/configuration.nix ~/.dotfiles/nixos/etc/nixos/'
+abbr rofirc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/rofi/config.rasi) && popd'
+abbr sshrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.ssh/config) && popd'
+abbr swayrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/sway/config) && popd'
+abbr tridactylrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/tridactyl/tridactylrc) && popd'
+abbr weztermrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/wezterm/wezterm.lua) && popd'
+abbr xplrrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/xplr/init.lua) && popd'
+abbr zellijrc 'pushd ~/.dotfiles && $EDITOR $(readlink -f ~/.config/zellij/config.kdl) && popd'
 
-# abbr -a -U for pip
-abbr -a -U pipu "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+# abbr for pip
+abbr pipu "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 
 # https://www.cyberciti.biz/faq/how-to-find-my-public-ip-address-from-command-line-on-a-linux/
-abbr -a -U myip "dig +short myip.opendns.com @resolver1.opendns.com"
+abbr myip "dig +short myip.opendns.com @resolver1.opendns.com"
 
-abbr -a -U tp btop
-abbr -a -U htop btop
-abbr -a -U top btop
+abbr tp btop
+abbr htop btop
+abbr top btop
 
-abbr -a -U lc 'libreoffice --calc'
+abbr lc 'libreoffice --calc'
 
-abbr -a -U loc 'scc --sort lines'
-abbr -a -U scc 'scc --sort lines'
-abbr -a -U tokei 'tokei --sort lines'
+abbr loc 'scc --sort lines'
+abbr scc 'scc --sort lines'
+abbr tokei 'tokei --sort lines'
 
-# rsync abbr -a -U
-abbr -a -U rsync 'rsync --archive --hard-links --compress --human-readable --info=progress2 --update'
-abbr -a -U rs 'rsync --archive --hard-links --compress --human-readable --info=progress2 --update'
+# rsync abbr
+abbr rsync 'rsync --archive --hard-links --compress --human-readable --info=progress2 --update'
+abbr rs 'rsync --archive --hard-links --compress --human-readable --info=progress2 --update'
 # }}}
 
-abbr -a -U rm trash-put
+abbr rm trash-put
 
 
-abbr -a -U rsc 'rustscan --addresses 192.168.1.0/24 --ulimit 5000 --ports 22 --greppable'
+abbr rsc 'rustscan --addresses 192.168.1.0/24 --ulimit 5000 --ports 22 --greppable'
 
-abbr -a -U adoc asciidoctor
-abbr -a -U adoc-pdf asciidoctor-pdf
+abbr adoc asciidoctor
+abbr adoc-pdf asciidoctor-pdf
