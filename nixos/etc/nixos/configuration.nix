59bc87f538188e20f75ce3b08caea12b78a72cba
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-04-08 21:44:05 +0200

    fix(nixos+sway): Fix geoclue2 first execution

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -291,10 +291,17 @@ in {
     # Configure podman to be use by minikube
     extraRules = [{
       users = [ "mimame" ];
-      commands = [{
-        command = "/run/current-system/sw/bin/podman";
-        options = [ "NOPASSWD" ];
-      }];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/podman";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command =
+            "/run/current-system/sw/bin/systemctl restart geoclue.service";
+          options = [ "NOPASSWD" ];
+        }
+      ];
     }];
   };
   # GNOME Keyring daemon
