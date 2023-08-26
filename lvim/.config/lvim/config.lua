62d5c6fc9acea3377bca4e831cf95c5b8bac938c
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-08-26 14:11:04 +0200

    feat(lvim): Add vim-surround plugin

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -332,6 +332,14 @@ lvim.plugins = {
   },
   -- enable repeating supported plugin maps with "."
   { "tpope/vim-repeat" },
+  -- mappings to delete, change and add surroundings
+  {
+    "tpope/vim-surround",
+    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+    -- setup = function()
+    --  vim.o.timeoutlen = 500
+    -- end
+  },
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
