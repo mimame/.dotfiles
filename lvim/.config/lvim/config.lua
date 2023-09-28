885a210c262ffdf490477883f4a5985aa91847e7
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-09-28 17:46:47 +0200

    fix(lvim): Remove wrong and failing automatic_installation (always done by default)

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,3 +1,7 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
 --[[
  THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
  `lvim` is the global options object
@@ -55,7 +59,6 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.autopairs.active = true
-lvim.lsp.installer.setup.automatic_installation = true
 
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
