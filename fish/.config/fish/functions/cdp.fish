5bd42c812cdbbccfaa3de9e5744272d01f9c62ba
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-10-02 19:04:37 +0200

    fix(fish): Fix cdp function with wayland-clipboard instead X11-clipboard (xclip)

--- fish/.config/fish/functions/cdp.fish
+++ fish/.config/fish/functions/cdp.fish
@@ -1,6 +1,6 @@
 # cd to the clipboard path
 function cdp
-    set clipboard_path (xclip -out)
+    set clipboard_path (wl-paste)
     echo "$clipboard_path"
     if test -d "$clipboard_path"
         cd "$clipboard_path"
