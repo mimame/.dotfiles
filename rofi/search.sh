15059a1a7fff6b0721cdd2756dffa49fc7c90378
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-10-13 15:54:34 +0200

    fix(rofi): Fix web search language

--- rofi/search.sh
+++ rofi/search.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" ]; then
-  (vivaldi --incognito https://www.google.com/search?q="$1" &)
+  (vivaldi --incognito "https://google.com/search?hl=en&q=$1" &)
   # Corner case: Try to open a url without any Vivaldi instance opened
   # Result: rofi is never closed by itself
   if ! "pgrep --full vivaldi"; then
