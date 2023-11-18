b7e73d4988ff0d2aff358fa79093fa0a9138ac8f
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2023-11-18 22:38:41 +0100

    fix(lvim): Fix nvim-metals

--- lvim/config.lua
+++ lvim/config.lua
@@ -392,6 +392,43 @@ lvim.plugins = {
   -- Metals plugin
   {
     "scalameta/nvim-metals",
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local metals_config = require("metals").bare_config()
+
+      -- Example of settings
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+        useGlobalExecutable = true, -- For NixOS, use system metals binary
+      }
+
+      -- *READ THIS*
+      -- I *highly* recommend setting statusBarProvider to true, however if you do,
+      -- you *have* to have a setting to display this in your statusline or else
+      -- you'll not see any messages from metals. There is more info in the help
+      -- docs about this
+      metals_config.init_options.statusBarProvider = "on"
+
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Autocmd that will actually be in charging of starting the whole thing
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
   },
   -- Neovim plugin for Obsidian
   {
