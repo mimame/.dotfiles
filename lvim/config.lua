cb8c4c3809077c28342e55cc3a64223259790a40
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-02-06 10:54:29 +0100

    feat(lvim): Improve obsidian.nvim config

--- lvim/config.lua
+++ lvim/config.lua
@@ -454,9 +454,17 @@ lvim.plugins = {
 
       -- Required Default vault
       require("obsidian").setup({
-        -- dir = "~/Documents/SecondBrain",
+        -- dir = "~/Documents/Brain",
         --
         disable_frontmatter = true,
+        -- Optional, for templates (see below).
+        templates = {
+          subdir = "02.Templates",
+          date_format = "%Y.%m-%d",
+          time_format = "%H:%M",
+          -- A map for custom variables, the key should be the variable and the value a function
+          substitutions = {},
+        },
         -- Don't use any kind of Zettelkasten ID format
         -- Simply use the title as filename
         note_id_func = function(title)
@@ -464,15 +472,15 @@ lvim.plugins = {
         end,
         workspaces = {
           {
-            name = "SecondBrain",
-            path = "~/Documents/SecondBrain",
+            name = "Brain",
+            path = "~/Documents/Brain",
           },
         },
         daily_notes = {
           -- Optional, if you keep daily notes in a separate directory.
-          folder = "00 Bullet Journal (Inbox)",
+          folder = "00.Bullet-Journal-Inbox",
           -- Optional, if you want to change the date format for the ID of daily notes.
-          date_format = "%d-%m-%Y",
+          date_format = "%y.%m.%d.%a",
           -- Optional, if you want to change the date format of the default alias of daily notes.
           alias_format = "%B %-d, %Y",
           -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
@@ -498,14 +506,64 @@ lvim.plugins = {
           --  * "notes_subdir" - put new notes in the default notes subdirectory.
           new_notes_location = "current_dir",
 
-          -- Whether to add the output of the node_id_func to new notes in autocompletion.
+          -- Control how wiki links are completed with these options:
+
+          -- Whether to add the note ID during completion.
           -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-          prepend_note_id = false
+          -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
+          prepend_note_id = true,
+
+          -- Whether to add the note path during completion.
+          -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
+          -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
+          prepend_note_path = false,
+
+          -- Whether to only use paths during completion.
+          -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
+          -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
+          use_path_only = false,
+        },
+        -- Optional, configure additional syntax highlighting / extmarks.
+        ui = {
+          enable = true,         -- set to false to disable all additional syntax features
+          update_debounce = 200, -- update delay after a text change (in milliseconds)
+          -- Define how various check-boxes are displayed
+          checkboxes = {
+            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+            -- Replace the above with this if you don't have a patched font:
+            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+            -- You can also add more custom ones...
+          },
+          external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          -- Replace the above with this if you don't have a patched font:
+          -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          reference_text = { hl_group = "ObsidianRefText" },
+          highlight_text = { hl_group = "ObsidianHighlightText" },
+          tags = { hl_group = "ObsidianTag" },
+          hl_groups = {
+            -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+            ObsidianTodo = { bold = true, fg = "#f78c6c" },
+            ObsidianDone = { bold = true, fg = "#89ddff" },
+            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+            ObsidianTilde = { bold = true, fg = "#ff5370" },
+            ObsidianRefText = { underline = true, fg = "#c792ea" },
+            ObsidianExtLinkIcon = { fg = "#c792ea" },
+            ObsidianTag = { italic = true, fg = "#89ddff" },
+            ObsidianHighlightText = { bg = "#75662e" },
+          },
         },
       })
 
       -- Syntax highlighting
       vim.g.vim_markdown_frontmatter = 1
+      vim.g.vim_markdown_follow_anchor = 1
+      vim.g.vim_markdown_folding_disabled = 1
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "markdown", "markdown_inline" },
         highlight = {
