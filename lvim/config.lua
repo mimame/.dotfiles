243d8a1d925d4e2a1e44446d901549163ba477b2
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-04-01 12:26:42 +0200

    fix(lvim): Fix deprecated attributes

--- lvim/config.lua
+++ lvim/config.lua
@@ -505,23 +505,21 @@ lvim.plugins = {
           --  * "current_dir" - put new notes in same directory as the current buffer.
           --  * "notes_subdir" - put new notes in the default notes subdirectory.
           new_notes_location = "current_dir",
-
-          -- Control how wiki links are completed with these options:
-
-          -- Whether to add the note ID during completion.
-          -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-          -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-          prepend_note_id = true,
-
-          -- Whether to add the note path during completion.
-          -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
-          -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-          prepend_note_path = false,
-
-          -- Whether to only use paths during completion.
-          -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
-          -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-          use_path_only = false,
+          -- Optional, customize how wiki links are formatted. You can set this to one of:
+          --  * "use_alias_only", e.g. '[[Foo Bar]]'
+          --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+          --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+          --  * "use_path_only", e.g. '[[foo-bar.md]]'
+          -- Or you can set it to a function that takes a table of options and returns a string, like this:
+          wiki_link_func = function(opts)
+            if opts.id == nil then
+              return string.format("[[%s]]", opts.label)
+            elseif opts.label ~= opts.id then
+              return string.format("[[%s|%s]]", opts.id, opts.label)
+            else
+              return string.format("[[%s]]", opts.id)
+            end
+          end,
         },
         -- Optional, configure additional syntax highlighting / extmarks.
         ui = {
