edfa6c7d7e93d650ff79cd8c6e4de8aa52622a3a
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-04-15 18:43:51 +0200

    fix(fish): Rewrite cb.fish to be compatible with Darwin too

--- fish/functions/cb.fish
+++ fish/functions/cb.fish
@@ -1,37 +1,34 @@
-# cb STRING to copy to the clipboard
-# cb FILE to copy to the clipboard
-# echo string | cb to copy to the clipboard
-# cb to paste from the clipboard
-# cb | command to paste from the clipboad
+# The 'cb' function is a versatile clipboard utility that adapts to both macOS and Linux environments.
+# It can be used in the following ways:
+#
+# - `cb STRING`: Copies the provided string to the clipboard.
+# - `cb FILE`: Copies the contents of the specified file to the clipboard.
+# - `echo STRING | cb`: Pipes a string into 'cb', copying it to the clipboard.
+# - `cb`: Outputs the current contents of the clipboard to stdout.
+# - `cb | COMMAND`: Pastes the contents of the clipboard into another command.
+#
+# The function automatically detects the operating system and uses 'pbcopy' and 'pbpaste' on macOS,
+# or 'wl-copy' and 'wl-paste' on Linux with Wayland.
 function cb
-    # stdin is a pipe
+    # Determine the appropriate copy and paste commands based on the OS
+    set copy_command (if test (uname) = Darwin; echo pbcopy; else; echo wl-copy; end)
+    set paste_command (if test (uname) = Darwin; echo pbpaste; else; echo wl-paste; end)
+
+    # If stdin is a pipe, read from stdin and copy to clipboard
     # https://github.com/fish-shell/fish-shell/issues/3792#issuecomment-276206686
-    # stdin -> clipboard
-    set copy_command wl-copy
-    set paste_command wl-paste
-    if test $(uname) = Darwin
-        set copy_command pbcopy
-        set paste_command pbpaste
-    end
     if command test -p /dev/stdin
-        set -l stdin_list
-        while read line
-            set stdin_list $stdin_list $line
-        end
-        $copy_command $stdin_list
-        # stdin is not a pipe
-    else if ! test -z "$argv[1]"
-        # file -> clipboard
+        cat - | $copy_command
+        # If arguments are provided, check if it's a file or a string
+    else if set -q argv[1]
+        # If the first argument is a file, copy its contents to the clipboard
         if test -f "$argv[1]"
-            $copy_command <"$argv[1]"
-            # string -> clipboard
+            cat "$argv[1]" | $copy_command
+            # Otherwise, treat the arguments as a string and copy to the clipboard
         else
-            $copy_command "$argv"
+            echo -n $argv | $copy_command
         end
+        # If no arguments and not a pipe, paste from clipboard to stdout
     else
-        # clipboard -> stdout
-        # no arguments were passed
-        # $paste_command
+        $paste_command
     end
-    $paste_command
 end
