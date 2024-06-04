6f612b68b1fada7a7d601071a5be365bcdfce7bf
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-06-04 21:37:12 +0200

    feat(nvchad): Map ENTER and ESC from insert mode

--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -5,6 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ENTER>")
+map("i", "ff", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
