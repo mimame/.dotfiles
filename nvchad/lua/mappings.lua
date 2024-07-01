1fee13f0c6b629009b36767627584b7b9a610527
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-07-01 20:49:19 +0200

    fix(nvchad): Use `aa` as esc in insert mode instead of `ff`

--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -6,6 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ENTER>")
-map("i", "ff", "<ESC>")
+map("i", "aa", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
