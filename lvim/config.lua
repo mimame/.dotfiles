9042c00e6883584be2d6f823429a4f27502801a3
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-07-16 11:03:02 +0200

    feat(lvim): Add dressing plugin

--- lvim/config.lua
+++ lvim/config.lua
@@ -190,6 +190,11 @@ lvim.plugins = {
 		priority = 1000,
 		config = true,
 	},
+	-- Improve the default vim.ui interfaces
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
 	-- Lightweight yet powerful formatter plugin
 	{
 		"stevearc/conform.nvim",
