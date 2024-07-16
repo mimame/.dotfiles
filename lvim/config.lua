9d084bfea914d25c1f61dc7b6808229dbb9af459
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-07-16 11:04:18 +0200

    feat(lvim): Add markdown-preview plugin

--- lvim/config.lua
+++ lvim/config.lua
@@ -576,6 +576,15 @@ lvim.plugins = {
 			})
 		end,
 	},
+	-- Markdown preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 	-- Neovim plugin for Obsidian
 	{
 		"epwalsh/obsidian.nvim",
