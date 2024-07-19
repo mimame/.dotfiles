39a7341723071778e1828edb7f9375f54dc86b9f
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-07-19 20:06:34 +0200

    feat(nvchad): Add flash plugin

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -117,6 +117,54 @@ return {
       },
     },
   },
+  -- Navigate your code with search labels, enhanced character motions and Treesitter integration
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
   -- Add/change/delete surrounding delimiter pairs with ease
   {
     "echasnovski/mini.surround",
