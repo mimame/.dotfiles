ba604c4a9ea7e13799c6d8697c9764d3d4ed1aee
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-07-21 13:26:38 +0200

    feat(nixos): Improve libvirtd service config

--- nixos/base/devops.nix
+++ nixos/base/devops.nix
@@ -27,7 +27,24 @@
       };
       guest.enable = false;
     };
-    libvirtd.enable = true;
+    # https://nixos.wiki/wiki/Libvirt
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.unstable.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
+      };
+    };
   };
 
   programs.singularity = {
