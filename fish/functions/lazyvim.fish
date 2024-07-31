cd8d82e9eab0fbac64ca0a3b62b8bdefd5f702dd
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2024-07-31 11:12:47 +0200

    fix(bin): Switch nvchad and lazyvim from fish functions to binaries

--- fish/functions/lazyvim.fish
+++
@@ -1,8 +0,0 @@
-function lazyvim
-    set -x NVIM_APPNAME lazyvim
-    if test (count $argv) -eq 0
-        nvim
-    else
-        nvim "$argv"
-    end
-end
--- fish/functions/nvchad.fish
+++
@@ -1,8 +0,0 @@
-function nvchad
-    set -x NVIM_APPNAME nvchad
-    if test (count $argv) -eq 0
-        nvim
-    else
-        nvim "$argv"
-    end
-end
