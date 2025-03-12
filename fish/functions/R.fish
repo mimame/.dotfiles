a9735004d6ac6af50a587a77da90b30e6ad9403b
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-03-12 17:20:28 +0100

    fix(fish): Remove deprecated functions

--- fish/functions/R.fish
+++
@@ -1,9 +0,0 @@
-function R
-    # stdin is a pipe
-    if command test -p /dev/stdin
-        command R "$argv"
-    else
-        # Use command to avoid the recursion
-        test -z "$argv" && radian || command R "$argv"
-    end
-end
--- fish/functions/ranger-cd.fish
+++
@@ -1,8 +0,0 @@
-function ranger-cd
-    set dir (mktemp -t ranger_cd.XXX)
-    ranger --choosedir=$dir
-    cd (cat $dir) $argv
-    rm $dir
-    commandline -f repaint
-end
-bind -M insert \co ranger-cd
