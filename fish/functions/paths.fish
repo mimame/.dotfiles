fce676b245e6fc1412c3d23280a55965c80aa395
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-03-12 17:19:31 +0100

    feat(fish): Improve paths.fish

--- fish/functions/paths.fish
+++ fish/functions/paths.fish
@@ -1,3 +1,3 @@
-function paths
-    echo "$PATH" | sed "s/:/\n/g" | nl | tac
+function paths --description "Display PATH entries in reverse order with line numbers"
+    string split : $PATH | nl -v 1 -w 3 | sort -nr
 end
