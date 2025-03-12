9f104395dadb77b129b85677839343e29c64d265
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-03-12 17:15:18 +0100

    feat(fish): Improve o.fish

--- fish/functions/o.fish
+++ fish/functions/o.fish
@@ -1,16 +1,25 @@
 # o function and open function with handlr
-# It works better than xdg-open in i3-wm and also it provides a better and nicer terminal interface than their xdg-utils equivalents
-function o
+# It works better than xdg-open and also it provides a better and nicer terminal interface than their xdg-utils equivalents
+function o --description "Open files/directories with their default applications using handlr"
     if test (count $argv) -eq 0
-        vifm .
+        y .
     else
-        # handlr can open multiple files at the same time without the explict loop
-        # but with a loop it's better to set nohup each std and err outputs independently
-        # to check faster if a file is not running fine
-        for file in "$argv"
-            # Run handlr with nohup in background and remove it from the jobs table
-            handlr open "$file" 2>/dev/null &
-            disown
+        set -l opened 0
+        for file in $argv
+            # Check if file exists (or is a valid URI)
+            if test -e "$file"; or string match -rq '^[a-zA-Z]+://' "$file"
+                # Run handlr with nohup in background and remove it from the jobs table
+                handlr open "$file" >/dev/null 2>&1 &
+                disown
+                set opened (math $opened + 1)
+            else
+                echo "Error: '$file' does not exist" >&2
+            end
+        end
+
+        # Provide feedback about how many files were opened
+        if test $opened -gt 0
+            test $opened -eq 1 && echo "Opened 1 item" || echo "Opened $opened items"
         end
     end
 end
