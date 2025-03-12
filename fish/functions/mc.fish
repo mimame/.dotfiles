8a92dc9d696c49f52da15a38d8e15d409b290b08
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-03-12 17:02:53 +0100

    feat(fish): Improve mc.fish

--- fish/functions/mc.fish
+++ fish/functions/mc.fish
@@ -1,3 +1,17 @@
-function mc
-    mkdir -pv "$argv" && cd "$argv"
+function mc --description "Make directory and change into it"
+    if test (count $argv) -eq 0
+        echo "Usage: mc DIRECTORY"
+        return 1
+    end
+
+    if test (count $argv) -gt 1
+        echo "Error: Too many arguments, expected one directory" >&2
+        return 1
+    end
+
+    if mkdir -pv "$argv[1]"
+        cd "$argv[1]"
+    else
+        return $status
+    end
 end
