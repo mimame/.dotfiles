f52e1e7b6baef4245095ce66f997b68bef33e2bb
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-03-12 15:44:59 +0100

    feat(fish): Improve ff.fish

--- fish/functions/ff.fish
+++ fish/functions/ff.fish
@@ -1,3 +1,9 @@
-function ff
-    floorp --private-window "https://www.google.com/search?q=$argv" &
+function ff --description "Open private Firefox window with DuckDuckGo search"
+    # Launch Firefox in background and detach from shell
+    if test -n "$argv"
+        firefox --private-window "https://duckduckgo.com/?q=$argv" &>/dev/null & disown
+    else
+        # If no query, just open DuckDuckGo
+        firefox --private-window "https://duckduckgo.com/" &>/dev/null & disown
+    end
 end
