7a7a6c4d51b4d68c798daec13a992da837f56f48
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-03-12 13:09:52 +0100

    feat(fish): Improve bb.fish

--- fish/functions/bb.fish
+++ fish/functions/bb.fish
@@ -1,30 +1,36 @@
-function bb
-    # Function to get all parent directories of a given path
-    function get_parent_dirs
-        # Determine the starting path
-        set current_path (realpath (test -z "$argv"; and echo "$PWD"; or echo "$argv"))
+function bb --description "Interactively navigate up the directory tree"
+    # Get the absolute path of the starting directory
+    set start_path (realpath (test -n "$argv"; and echo "$argv"; or echo "$PWD"))
 
-        # Initialize an empty list for directories
-        set -l dirs
+    # Create a list of parent directories
+    set -l dirs
+    set current_path $start_path
 
-        # Traverse up the directory tree until the root is reached
-        while test "$current_path" != /
-            # Move to the parent directory
-            set current_path (dirname "$current_path")
-
-            # Add the directory to the list if it exists
-            if test -d "$current_path"
-                set -a dirs $current_path
-            end
-        end
-
-        # Print the list of directories, each on a new line
-        printf "%s\n" $dirs
+    # Add current directory first if not explicitly provided a path
+    if test -z "$argv"
+        set -a dirs $current_path
     end
 
-    # Use get_parent_dirs to list directories and navigate to the selected one
-    set final_dir (get_parent_dirs "$argv" | nl -v 1 | fzf | cut -f2)
+    # Traverse up the directory tree until the root is reached
+    while test "$current_path" != /
+        # Move to the parent directory
+        set current_path (dirname "$current_path")
 
-    # Change to the selected directory
-    cd "$final_dir"
+        # Add the directory to the list if it exists
+        if test -d "$current_path"
+            set -a dirs $current_path
+        end
+    end
+
+    # Use fzf to select a directory with preview
+    # set final_dir (printf "%s\n" $dirs | fzf)
+    set final_dir (printf "%s\n" $dirs |
+        fzf --reverse --prompt="Navigate up: " \
+            --preview="eza --sort .name --color=always --links --git --icons --classify --extended {}" \
+            --header="Select a parent directory")
+
+    # Change to the selected directory if one was chosen
+    if test -n "$final_dir" -a -d "$final_dir"
+        cd "$final_dir"
+    end
 end
