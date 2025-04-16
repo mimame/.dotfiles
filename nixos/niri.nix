7b982796ac41db493e3127938d5bac0231a410d1
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-04-16 18:02:39 +0200

    feat(niri): Add systemd user services for gammastep and swayidle

--- nixos/niri.nix
+++ nixos/niri.nix
@@ -24,13 +24,46 @@ in
     };
   };
 
+  # systemd units
+  systemd = {
+    user.services = {
+      gammastep = {
+        enable = true;
+        description = "gammastep";
+        wantedBy = [ "niri.service" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.gammastep}/bin/gammastep";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+      swayidle = {
+        enable = true;
+        description = "swayidle";
+        wantedBy = [ "niri.service" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.unstable.swayidle}/bin/swayidle timeout 3600 'systemctl suspend-then-hibernate'";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+    };
+  };
+
   # Whether to run XDG autostart files for sessions without a desktop manager (with only a window manager), these sessions usually don’t handle XDG autostart files by default.
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
 
   environment.systemPackages =
     with pkgs;
     [
-
       xwayland-satellite
     ]
     ++ (with pkgs.unstable; [
@@ -39,7 +72,6 @@ in
       swayosd
       swayr
       walker
-      waybar
       waybar
 
     ]);
