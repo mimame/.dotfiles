4ad0015e2e5580eaf689df7abc07729736c4397c
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-05-20 09:23:14 +0200

    feat(nixos): Upgrade Lix to the latest version

--- nixos/core/lix.nix
+++ nixos/core/lix.nix
@@ -3,25 +3,25 @@
 #
 # The sha256 hashes were obtained with the following command in Lix (n.b.
 # this relies on --unpack, which is only in Lix and CppNix > 2.18):
-# nix store prefetch-file --name source --unpack https://git.lix.systems/lix-project/lix/archive/2.92.0.tar.gz
+# nix store prefetch-file --name source --unpack https://git.lix.systems/lix-project/lix/archive/2.93.0.tar.gz
 #
-# Note that the tag (e.g. 2.92.0) in the URL here is what determines
+# Note that the tag (e.g. 2.93.0) in the URL here is what determines
 # which version of Lix you'll wind up with.
 (
   let
     module = fetchTarball {
       name = "source";
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
-      sha256 = "sha256-fprwZKE1uMzO9tiWWOrmLWBW3GPkMayQfb0xOvVFIno=";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      sha256 = "sha256-11R4K3iAx4tLXjUs+hQ5K90JwDABD/XHhsM9nkeS5N8=";
     };
     lixSrc = fetchTarball {
       name = "source";
-      url = "https://git.lix.systems/lix-project/lix/archive/2.92.0.tar.gz";
-      sha256 = "sha256-CCKIAE84dzkrnlxJCKFyffAxP3yfsOAbdvydUGqq24g=";
+      url = "https://git.lix.systems/lix-project/lix/archive/2.93.0.tar.gz";
+      sha256 = "sha256-hsFe4Tsqqg4l+FfQWphDtjC79WzNCZbEFhHI8j2KJzw=";
     };
+    # This is the core of the code you need; it is an exercise to the
+    # reader to write the sources in a nicer way, or by using npins or
+    # similar pinning tools.
   in
-  # This is the core of the code you need; it is an exercise to the
-  # reader to write the sources in a nicer way, or by using npins or
-  # similar pinning tools.
   import "${module}/module.nix" { lix = lixSrc; }
 )
