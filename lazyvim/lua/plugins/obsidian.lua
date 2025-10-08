24dba45256f3ff055dbfc0e645d9cec8bd5d440a
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-10-08 16:15:18 +0200

    feat(lazyvim): enable sidekick and configure obsidian
    Enable the sidekick AI plugin and adjust the Obsidian configuration.
    
    The sidekick plugin is enabled in the lazy.lua config.
    
    The Obsidian configuration is updated to:
    - Change the templates directory to 3.Templates.
    - Change the daily notes directory to 1.Journal.
    - Remove the custom UI configuration, reverting to the default UI.

--- lazyvim/lua/plugins/obsidian.lua
+++ lazyvim/lua/plugins/obsidian.lua
@@ -26,7 +26,7 @@ return {
         disable_frontmatter = true,
         -- Optional, for templates (see below).
         templates = {
-          subdir = "02.Templates",
+          subdir = "3.Templates",
           date_format = "%Y.%m-%d",
           time_format = "%H:%M",
           -- A map for custom variables, the key should be the variable and the value a function
@@ -45,22 +45,13 @@ return {
         },
         daily_notes = {
           -- Optional, if you keep daily notes in a separate directory.
-          folder = "00.Bullet-Journal-Inbox",
+          folder = "1.Journal",
           -- Optional, if you want to change the date format for the ID of daily notes.
           date_format = "%y.%m.%d.%a",
           -- Optional, if you want to change the date format of the default alias of daily notes.
           alias_format = "%B %-d, %Y",
           -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
           template = nil,
-        },
-        mappings = {
-          -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-          -- ["gf"] = {
-          --   action = function()
-          --     return require("obsidian").util.gf_passthrough()
-          --   end,
-          --   opts = { noremap = false, expr = true, buffer = true },
-          -- },
         },
         -- Optional, completion.
         completion = {
@@ -87,37 +78,6 @@ return {
         -- Optional, configure additional syntax highlighting / extmarks.
         ui = {
           enable = true, -- set to false to disable all additional syntax features
-          update_debounce = 200, -- update delay after a text change (in milliseconds)
-          -- Define how various check-boxes are displayed
-          checkboxes = {
-            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-            ["x"] = { char = "", hl_group = "ObsidianDone" },
-            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-            -- Replace the above with this if you don't have a patched font:
-            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
-            -- You can also add more custom ones...
-          },
-          external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-          -- Replace the above with this if you don't have a patched font:
-          -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-          reference_text = { hl_group = "ObsidianRefText" },
-          highlight_text = { hl_group = "ObsidianHighlightText" },
-          tags = { hl_group = "ObsidianTag" },
-          hl_groups = {
-            -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-            ObsidianTodo = { bold = true, fg = "#f78c6c" },
-            ObsidianDone = { bold = true, fg = "#89ddff" },
-            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-            ObsidianTilde = { bold = true, fg = "#ff5370" },
-            ObsidianRefText = { underline = true, fg = "#c792ea" },
-            ObsidianExtLinkIcon = { fg = "#c792ea" },
-            ObsidianTag = { italic = true, fg = "#89ddff" },
-            ObsidianHighlightText = { bg = "#75662e" },
-          },
         },
       })
 
@@ -125,13 +85,6 @@ return {
       vim.g.vim_markdown_frontmatter = 1
       vim.g.vim_markdown_follow_anchor = 1
       vim.g.vim_markdown_folding_disabled = 1
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "markdown", "markdown_inline" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { "markdown" },
-        },
-      })
     end,
   },
 }
