b677ed603c97fef49bfee5b23b5abd25c9ae78ae
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2025-12-26 13:24:15 -0600

    chore(fish): remove custom ssh wrapper in favor of ghostty integration
    Remove the custom ssh wrapper function for WezTerm and Kitty terminals.
    As Ghostty is now my default terminal, I am leveraging its native
    `ssh-env` and `ssh-terminfo` shell integration features, which provide
    a more robust experience than a manual wrapper.

--- fish/functions/_ssh.fish
+++
@@ -1,9 +0,0 @@
-function ssh --description "SSH for wezterm or Kitty terminals"
-    if test "$TERM" = wezterm
-        wezterm ssh "$argv"
-    else if test "$TERM" = xterm-kitty
-        kitty +kitten ssh "$argv"
-    else
-        command ssh "$argv"
-    end
-end
