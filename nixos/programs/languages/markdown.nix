c43c72f05930e5d0bf643b0edfdf164852d96150
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2026-01-24 13:25:57 +0100

    refactor(nixos): use stable inlyne and split markdown package list
    Move `inlyne` to the stable package set and refactor the package list in
    `markdown.nix` to combine stable and unstable packages using the list
    concatenation operator.

--- nixos/programs/languages/markdown.nix
+++ nixos/programs/languages/markdown.nix
@@ -5,11 +5,15 @@
 }:
 
 {
-  environment.systemPackages = with pkgs.unstable; [
-    markdownlint-cli2 # A fast, flexible, and configurable Markdown linter
-    marksman # Markdown language server
-    mdbook # A command line tool to create books from Markdown files
-    mdl # Markdown lint tool
-    inlyne # A fast and feature-rich Markdown viewer
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      inlyne # A fast and feature-rich Markdown viewer
+    ]
+    ++ (with pkgs.unstable; [
+      markdownlint-cli2 # A fast, flexible, and configurable Markdown linter
+      marksman # Markdown language server
+      mdbook # A command line tool to create books from Markdown files
+      mdl # Markdown lint tool
+    ]);
 }
