b501d33d0d0f9040c57e468e8fc1169e8838eb1c
Miguel Madrid-Mencía <miguel.madrid.mencia@gmail.com>
2026-01-27 17:28:00 +0100

    feat(nixos): add and document security tools
    Add age and sops for modern secret management and provide descriptive
    comments for all packages in the security configuration to improve
    maintainability.

--- nixos/programs/security/default.nix
+++ nixos/programs/security/default.nix
@@ -1,15 +1,21 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs.unstable; [
-    bitwarden-cli
-    gopass
-    gpg-tui
-    keychain
-    libsecret
-    sequoia-sq
-    vulnix
-    gitleaks
-    ripsecrets
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      age # Modern encryption tool
+      sops # SOPS: Secrets OPerationS
+    ]
+    ++ (with pkgs.unstable; [
+      bitwarden-cli # Command-line interface for Bitwarden
+      gopass # The standard unix password manager for teams
+      gpg-tui # Terminal user interface for GnuPG
+      keychain # A manager for ssh-agent and gpg-agent
+      libsecret # Library for storing and retrieving passwords and other secrets
+      sequoia-sq # Command-line frontend for Sequoia PGP
+      vulnix # Vulnerability scanner for Nix expressions and closures
+      gitleaks # Scan git repos for secrets
+      ripsecrets # Find secrets in source code
+    ]);
 }
